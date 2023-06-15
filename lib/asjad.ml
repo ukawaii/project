7d45e2c5f6985e5c396f7b2cf258c73bd1d6dfc6
ukawaii <huskateet@gmail.com>
2023-06-15 22:03:04 +0300

    vist null tööta

--- lib/asjad.ml
+++ lib/asjad.ml
@@ -1,6 +1,7 @@
 let is_lower_case c = let reg = Str.regexp "[a-z]" in Str.string_match reg c 0  ;;
 
 let tukelda s =
+  let s = String.trim s in
   let reg = Str.regexp "→" in (Str.split reg s);;
 
 let gram_to_list s = String.split_on_char '\n' s;;
@@ -11,7 +12,7 @@ type value_atom = True | False | Unknown [@@deriving show]
 type parem ={
   tervik : string;
   value : value_atom
-}[@@deriving show]
+}
 
 type terminal = {
   vasak_pool : string;
@@ -19,20 +20,19 @@ type terminal = {
 }
 
 let hash = Hashtbl.create 69;;
-(* let nullable = Hashtbl.create 69 ;;
- *)
+let nullable = Hashtbl.create 69 ;;
 let loo_parem_pool l =
   let l' = 
   if List.length l = 1 then List.append l ["ε"]
   else l
   in
-  let parem_pool = List.nth l' 1 in let parem_pool = String.trim parem_pool in if parem_pool = "" then {tervik = parem_pool; value = True} else
+  let parem_pool = List.nth l' 1 in let parem_pool = String.trim parem_pool in if parem_pool = "ε" then {tervik = parem_pool; value = True} else
   if is_lower_case parem_pool then {tervik = parem_pool; value = False} else {tervik = parem_pool; value = Unknown};;
 
 let loo_vasak_pool l = let vasak_pool = List.nth l 0 in let vasak_pool = String.trim vasak_pool in
 match Hashtbl.find_opt hash vasak_pool with
 | None -> Hashtbl.add hash vasak_pool [loo_parem_pool l]
-| _ -> Hashtbl.add hash vasak_pool ([loo_parem_pool l] @ (Hashtbl.find hash vasak_pool))
+| _ -> Hashtbl.replace hash vasak_pool ([loo_parem_pool l] @ (Hashtbl.find hash vasak_pool))
 ;;
 
 let loo_pooled l = List.iter (fun x -> loo_vasak_pool (tukelda x)) l
@@ -48,6 +48,31 @@ let rec concatParemList lis =
   | h :: t -> "" ^ h.tervik ^ "|" ^ concatParemList t
 
 
+let contains_unknown l = let mappo = List.map (fun x -> x.value = Unknown) l in List.mem true mappo ;;
 
-let asi () = let list = gram_to_list test_string in loo_pooled list; Hashtbl.iter (fun x y -> print_endline (x ^ "->" ^ (concatParemList y) ^ "\n")) hash
+
+(* Kui leidub 1 tõeväärtus siis on tõene, sest nullable väärtus on a v b v c *)
+let find_value l = let mappo = List.map (fun x -> x.value = True) l in List.mem true mappo ;;
+
+let print_terminal term table = let parem = Hashtbl.find table term in
+List.iter (fun x -> print_endline("Terminal: " ^ term ^ " ParemPool :" ^ x.tervik ^ " Väärtus :" ^ (show_value_atom x.value))) parem
+
+let print_nullable term table =print_endline ("Terminal: " ^ term ^ " Nullable: " ^  string_of_bool(Hashtbl.find table term));;
+
+let print_terminal_all table = Hashtbl.iter (fun x _ -> print_terminal x table) table;;
+
+let print_nullable_all table = Hashtbl.iter (fun x _ -> print_nullable x table) table;;
+
+let try_eval term parem_list = if not (contains_unknown parem_list) then
+  let value = find_value parem_list in Hashtbl.add nullable term value
+;;
+
+let rec find_nullable () =
+  if Hashtbl.length nullable = Hashtbl.length hash then nullable
+  else (Hashtbl.iter (fun x y -> try_eval x y) hash; find_nullable ())
+
+  
+
+let asi () = let list = gram_to_list test_string in loo_pooled list; Hashtbl.iter (fun x y -> print_endline (x ^ "->" ^ (concatParemList y) ^ "\n")) hash;
+print_nullable_all (find_nullable());
  ;;
--- test/test.ml
+++ test/test.ml
@@ -3,9 +3,9 @@ open OUnit2
 
 let tests = "General Test" >::: [
   "Something" >:: (fun _ -> asi ());
-  "Empty rule" >:: (fun _ -> let parem = (loo_parem_pool "T → ") in assert_equal parem.value True);
-  "Terminal rule" >:: (fun _ -> let parem = (loo_parem_pool "T → a") in assert_equal parem.value False);
-  "NonTerminal rule" >:: (fun _ -> let parem = (loo_parem_pool "T → R") in assert_equal parem.value Unknown);
+  "Empty rule" >:: (fun _ -> let parem = (let s = tukelda "T → " in (loo_parem_pool s)) in assert_equal parem.value True);
+  "Terminal rule" >:: (fun _ -> let parem = (let s = tukelda "T → a" in (loo_parem_pool s)) in assert_equal parem.value False);
+  "NonTerminal rule" >:: (fun _ -> let parem = (let s = tukelda "T → R" in (loo_parem_pool s)) in assert_equal parem.value Unknown);
 ]
 
 let _ = run_test_tt_main tests
